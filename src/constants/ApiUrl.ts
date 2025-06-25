1fbc402eab3cff08cf3caf42623b3a435e75a060
GabrielSetta <devsetta@gmail.com>
2025-06-24 21:53:25 -0300

    Update APi

--- src/constants/ApiUrl.ts
+++ src/constants/ApiUrl.ts
@@ -1,5 +1,8 @@
 //local
-export const API_URL = 'https://handyman-back-production.up.railway.app';
+export const API_URL = 'http://10.0.2.2:3003';
+
+// Para dispositivo físico, use o IP da sua máquina, exemplo:
+// export const API_URL = 'http://SEU_IP_LOCAL:3003';
 
 // https://handyman-back-production.up.railway.app
 
